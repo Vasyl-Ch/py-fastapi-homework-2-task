ef820114ec515e1e5ef3542d4cd0b65bb23db134
Vasyl Cherkes <cherkesva@gmail.com>
2026-01-13 19:02:46 +0200

    Solution

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 from routes import movie_router
 
@@ -8,6 +10,22 @@ app = FastAPI(
     description="Description of project"
 )
 
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+        request: Request,
+        exc: RequestValidationError
+):
+    if any(error["loc"][0] == "query" for error in exc.errors()):
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content={"detail": exc.errors()},
+        )
+
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={"detail": "Invalid input data."},
+    )
 api_version_prefix = "/api/v1"
 
 app.include_router(movie_router, prefix=f"{api_version_prefix}/theater", tags=["theater"])
